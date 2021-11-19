a457eefe4e9604dd0336e4424b3a476e08372f44
Your Name <hsynozcn009@gmail.com>
2021-11-19 14:01:23 +0300

    searchbar ve index of methodu filtreleme islemleri

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,10 +2,27 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
+    state = {
+        searchQuery: ""
+    }
+
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+    }
     render() {
 
         return  (
-            <h1>Search Bar</h1>
+            <form onSubmit={this.handleFormSubmit}>
+            <div className="form-row mb-5">
+                <div className="col-12">
+                    <input 
+                        onChange={this.props.searchMovieProp} 
+                        type="text" className="form-control" 
+                        placeholder="Seach a movie" 
+                    />
+                </div>
+            </div>
+        </form>
         )
 
     }
